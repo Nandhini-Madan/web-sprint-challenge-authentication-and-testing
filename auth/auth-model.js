b2be9857ef60e2fed8645ece56349472ec95f700
unknown <nandhinimadan20@gmail.com>
2020-11-07 18:56:54 -0800

    Completed login and register

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -6,3 +6,24 @@ function findUserName(username){
             .first("id","username","password")
 
 }
+function addUser(data){
+   const [id]=db("users").insert(data)
+   return findByUserId(id)
+}
+function findByUserId(id){
+    return db("users")
+            .where("id",id)
+            .first("id","username","password")
+}
+function find(){
+    
+}
+
+module.exports={
+    findUserName,
+    addUser,
+    findByUserId,
+
+
+
+}
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,40 @@
 const router = require('express').Router();
-const user=require("./auth-model")
+const user = require("./auth-model")
+const bycrypt = require("bcryptjs")
 
 router.post('/register', async (req, res) => {
   // implement registration
+  try {
+    const { username, password } = req.body
+    if (!username || !password) {
+      res.status(401).json({
+        message: "Username or password not available"
+      })
+    }
+    else {
+      const users = await user.findUserName(username)
+      if (!users) {
+        res.status(401).json({
+          message: "Username name is not available"
+        })
+      }
+      else {
+        const newUser = await user.addUser({
+          username,
+          password: await bycrypt.hash(password, 14)
+        })
+        res.status(201).json(newUser)
+      }
+
+    }
+  }
+  catch (err) {
+    next(err)
+  }
+});
+
+router.post('/login', async (req, res) => {
+  // implement login
   try{
     const {username,password}=req.body
     if(!username||!password){
@@ -11,17 +43,24 @@ router.post('/register', async (req, res) => {
       })
     }
     else{
-      const user=await user
-    //  const [id]=
+      const user =await user.findUserName(username)
+      if(!user){
+        res.status(401).json({
+          message:"Invalid User credentials"
+        })
+      }
+      else{
+        const passwordValid=await bycrypt.compare(password,user.password)
+        if(!passwordValid){
+        //  const token=
+          res.status(200).json({
+            message:`Welcome ${user.username}`
+          })
+        }
+      }
     }
   }
-  catch(err){
-
-  }
-});
-
-router.post('/login', (req, res) => {
-  // implement login
+  catch(err){}
 
 });
 
